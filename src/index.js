95f174b46642e75ae1efcb2c7b7a138b02901e02
Paula Manese <41752060+PaulaManese@users.noreply.github.com>
2022-08-06 20:20:07 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <div>
+    <h1 style={customStyle}>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Inline styling on css properties
+{
+  /* <h1 style={{color:"red"}}>My Favourite Foods</h1> */
+}
